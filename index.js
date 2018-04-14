c38cb227b595c1b2415f3f3d266818298aa1e426
Titus Wormer <tituswormer@gmail.com>
2018-04-14 11:59:12 +0200

    Refactor code-style

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-'use strict';
+'use strict'
 
-module.exports = iterate;
+module.exports = iterate
 
-var own = {}.hasOwnProperty;
+var own = {}.hasOwnProperty
 
 function iterate(values, callback, context) {
-  var index = -1;
-  var result;
+  var index = -1
+  var result
 
   if (!values) {
-    throw new Error('Iterate requires that |this| not be ' + values);
+    throw new Error('Iterate requires that |this| not be ' + values)
   }
 
   if (!own.call(values, 'length')) {
-    throw new Error('Iterate requires that |this| has a `length`');
+    throw new Error('Iterate requires that |this| has a `length`')
   }
 
   if (typeof callback !== 'function') {
-    throw new Error('`callback` must be a function');
+    throw new Error('`callback` must be a function')
   }
 
   /* The length might change, so we do not cache it. */
   while (++index < values.length) {
     /* Skip missing values. */
     if (!(index in values)) {
-      continue;
+      continue
     }
 
-    result = callback.call(context, values[index], index, values);
+    result = callback.call(context, values[index], index, values)
 
     /* If `callback` returns a `number`, move `index` over to
      * `number`. */
     if (typeof result === 'number') {
       /* Make sure that negative numbers do not break the loop. */
       if (result < 0) {
-        index = 0;
+        index = 0
       }
 
-      index = result - 1;
+      index = result - 1
     }
   }
 }
--- test.js
+++ test.js
@@ -1,116 +1,120 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var iterate = require('.');
+var test = require('tape')
+var iterate = require('.')
 
-test('iterate()', function (t) {
+test('iterate()', function(t) {
   t.throws(
-    function () {
-      iterate();
+    function() {
+      iterate()
     },
     /^Error: Iterate requires that \|this\| not be undefined$/,
     'should throw without `values`'
-  );
+  )
 
   t.throws(
-    function () {
-      iterate({});
+    function() {
+      iterate({})
     },
     /Error: Iterate requires that \|this\| has a `length`/,
     'should throw without `values.length`'
-  );
+  )
 
   t.throws(
-    function () {
-      iterate([]);
+    function() {
+      iterate([])
     },
     /^Error: `callback` must be a function$/,
     'should throw without `callback`'
-  );
+  )
 
-  t.test('should invoke `callback` each step', function (st) {
-    var list = [0, 1, 2];
-    var n = 0;
+  t.test('should invoke `callback` each step', function(st) {
+    var list = [0, 1, 2]
+    var n = 0
 
-    iterate(list, function (value, index, values) {
-      st.equal(value, n);
-      st.equal(index, n);
-      st.equal(values, list);
-      st.equal(this, undefined);
+    iterate(list, function(value, index, values) {
+      st.equal(value, n)
+      st.equal(index, n)
+      st.equal(values, list)
+      st.equal(this, undefined)
 
-      n++;
-    });
+      n++
+    })
 
-    st.equal(n, 3);
-    st.end();
-  });
+    st.equal(n, 3)
+    st.end()
+  })
 
-  t.test('should invoke `callback` with context', function (st) {
-    var self = this;
-    var n = 0;
+  t.test('should invoke `callback` with context', function(st) {
+    var self = this
+    var n = 0
 
-    iterate([1, 2, 3], function () {
-      st.equal(this, self);
-      n++;
-    }, self);
+    iterate(
+      [1, 2, 3],
+      function() {
+        st.equal(this, self)
+        n++
+      },
+      self
+    )
 
-    st.equal(n, 3);
-    st.end();
-  });
+    st.equal(n, 3)
+    st.end()
+  })
 
-  t.test('should use the given return value', function (st) {
-    var n = 0;
+  t.test('should use the given return value', function(st) {
+    var n = 0
 
-    iterate([0, 1, 2], function (value, index) {
-      n++;
+    iterate([0, 1, 2], function(value, index) {
+      n++
 
-      st.equal(value, index);
+      st.equal(value, index)
 
       /* Stay on position `0` ten times. */
       if (n <= 10) {
-        return 0;
+        return 0
       }
-    });
+    })
 
-    st.equal(n, 13);
+    st.equal(n, 13)
 
-    st.end();
-  });
+    st.end()
+  })
 
-  t.test('should ignore missing values', function (st) {
-    var magicNumber = 10;
-    var list = new Array(magicNumber);
-    var n;
+  t.test('should ignore missing values', function(st) {
+    var magicNumber = 10
+    var list = new Array(magicNumber)
+    var n
 
-    list.push(magicNumber + 1);
+    list.push(magicNumber + 1)
 
-    iterate(list, function (value, index) {
-      st.equal(value, magicNumber + 1);
-      st.equal(index, magicNumber);
-      n = index;
-    });
+    iterate(list, function(value, index) {
+      st.equal(value, magicNumber + 1)
+      st.equal(index, magicNumber)
+      n = index
+    })
 
-    st.equal(n, magicNumber);
+    st.equal(n, magicNumber)
 
-    st.end();
-  });
+    st.end()
+  })
 
-  t.test('should support negative indices', function (st) {
-    var n = 0;
-    var results = ['a', 'b', 'a', 'b', 'c', 'd'];
+  t.test('should support negative indices', function(st) {
+    var n = 0
+    var results = ['a', 'b', 'a', 'b', 'c', 'd']
 
-    iterate(['a', 'b', 'c', 'd'], function (value) {
-      st.equal(value, results[n]);
-      n++;
+    iterate(['a', 'b', 'c', 'd'], function(value) {
+      st.equal(value, results[n])
+      n++
 
       if (n === 2) {
-        return -1;
+        return -1
       }
-    });
+    })
 
-    st.equal(n, results.length);
-    st.end();
-  });
+    st.equal(n, results.length)
+    st.end()
+  })
 
-  t.end();
-});
+  t.end()
+})
